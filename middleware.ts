64b616e22d06cf8c37728b5d1e28c146c11b8b7b
B-priyam <priyam3801h@gmail.com>
2025-09-04 12:39:58 +0530

    initial commit

--- middleware.ts
+++ middleware.ts
@@ -2,23 +2,14 @@ import { cookies, headers } from "next/headers";
 import { redirect, usePathname } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
-const routes = [
-  "/login",
-  "/_next/static/css/app/layout.css",
-  "/_next/static/chunks/webpack.js",
-  "/_next/static/chunks/main-app.js",
-  "/_next/static/chunks/app-pages-internals.js",
-  "/_next/static/chunks/app/layout.js",
-  "/_next/static/chunks/app/login/page.js",
-  "/_next/static/chunks/_app-pages-browser_node_modules_next_dist_client_dev_noop-turbopack-hmr_js.js",
-];
+const routes = ["/"];
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   const token = await (await cookies()).get("id");
 
-  if (!routes.includes(pathname) && !token) {
+  if (routes.includes(pathname) && !token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   if (token && pathname === "/login") {
